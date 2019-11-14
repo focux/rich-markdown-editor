0cf442509168c24dbbec43e9e73f969e0970625e
Leonardo Dominguez <focuux@gmail.com>
2019-11-14 00:28:23 -0400

    remove more stuff related to the anchors on heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,7 +2,6 @@
 import * as React from "react";
 import styled from "styled-components";
 import type { SlateNodeProps } from "../types";
-import CopyToClipboard from "./CopyToClipboard";
 
 type Props = SlateNodeProps & {
   level: number,
@@ -29,65 +28,17 @@ function Heading(props: Props) {
   );
 }
 
-const CollapseToggle = styled.a`
-  text-decoration: none;
-  opacity: ${props => (props.disabled ? "0" : "1")};
-  pointer-events: ${props => (props.disabled ? "none" : "all")};
-  visibility: ${props => (props.collapsed ? "visible" : "hidden")};
-  user-select: none;
-  cursor: pointer;
-  width: 24px;
-  height: 24px;
-
-  svg {
-    ${props => props.collapsed && "transform: rotate(-90deg);"};
-    fill: ${props =>
-      props.collapsed ? props.theme.text : props.theme.placeholder};
-    transition: transform 100ms ease-in-out;
-  }
-
-  &:hover {
-    text-decoration: none;
-
-    svg {
-      fill: ${props => props.theme.text};
-    }
-  }
-`;
-
 const Wrapper = styled.div`
   display: inline;
   margin-left: ${(props: Props) => (props.hasPretitle ? "-1.2em" : 0)};
-`;
-
-const Anchor = styled(CopyToClipboard)`
-  visibility: hidden;
-  padding-left: 0.25em;
 `;
 
 export const StyledHeading = styled(Heading)`
   display: flex;
   align-items: center;
   position: relative;
-  margin-left: -24px;
-
-  &:hover {
-    ${CollapseToggle} {
-      visibility: visible;
-    }
-
-    ${Anchor} {
-      color: ${props => props.theme.placeholder};
-      visibility: visible;
-      text-decoration: none;
-      cursor: pointer;
-
-      &:hover {
-        color: ${props => props.theme.text};
-      }
-    }
-  }
 `;
+
 export const Heading1 = (props: SlateNodeProps) => (
   <StyledHeading level={1} {...props} />
 );
